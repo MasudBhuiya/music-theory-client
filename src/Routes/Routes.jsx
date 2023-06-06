8d3dcfe3d2750febe2a4c60d1271fdb42c2b1b58
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-06-06 21:29:38 +0600

    added footer

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,11 @@
 import {
   createBrowserRouter
 } from "react-router-dom";
+import Main from "../Layout/Main";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Main></Main>,
     },
   ]);
