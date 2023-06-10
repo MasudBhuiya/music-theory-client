3cc94fc0f39687f15296782b3c535c6515003387
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-06-10 17:24:21 +0600

    create admin

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -6,6 +6,8 @@ import Swal from 'sweetalert2';
 
 const AllUsers = () => {
     useTitle('All Users')
+
+    //Tanstack Query
     const {data: users = [], refetch} = useQuery(['users'], async()=> {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
